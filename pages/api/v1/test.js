cbd11bdbe3b524ed0af82e702781c06589a4fe40
Bruno Paula <devbrunopaula@gmail.com>
2021-06-12 18:29:09 -0400

    added

--- pages/api/v1/test.js
+++ pages/api/v1/test.js
@@ -4,11 +4,11 @@ export default async function (req, res) {
 	const leadBody = req.body
 
 	try {
-		const response = await api('post', '/webleads', leadBody)
-		// const response = await axios.post(
-		// 	'https://webhook.site/f0b26d96-5167-4664-a2dd-f9800b46bc90',
-		// 	leadBody
-		// )
+		// const response = await api('post', '/webleads', leadBody)
+		const response = await axios.post(
+			'https://webhook.site/f0b26d96-5167-4664-a2dd-f9800b46bc90',
+			leadBody
+		)
 		console.log(response)
 		res.status(201).json(response)
 	} catch (error) {
