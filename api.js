fcb3f0720ef56565c9208cec1658700175060f61
Bruno Paula <devbrunopaula@gmail.com>
2021-06-12 14:18:41 -0400

    added sm api endpoint

--- api.js
+++ api.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 const api = async (method, path, payload) => {
 	const options = {
 		method: method,
-		url: 'https://api.servicemonster.net/v1/' + path,
+		url: process.env.URL + path,
 		headers: {
-			Authorization: 'Basic UzZCazBRVjE6cVplSDJUNlJid0ZJQkRIemNpbA==',
+			Authorization: process.env.SM_TOKEN,
 		},
 		data: payload,
 	}
